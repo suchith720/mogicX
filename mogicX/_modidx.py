8799ae5f731feb5de2318956e6a78f5bb737f041
suchith <suchith720@gmail.com>
2025-03-26 16:35:08 +0530

    noise experiments

--- mogicX/_modidx.py
+++ mogicX/_modidx.py
@@ -28,4 +28,5 @@ d = { 'settings': { 'branch': 'main',
             'mogicX.09_oak-for-wikiseealsotitles-split-001': {},
             'mogicX.10_ngame-linker-for-wikiseealsotitles-noise': {},
             'mogicX.11_ngame-oracle-for-wikiseealsotitles-noise': {},
+            'mogicX.12_mogic-for-wikiseealsotitles-noise': {},
             'mogicX.core': {'mogicX.core.foo': ('core.html#foo', 'mogicX/core.py')}}}

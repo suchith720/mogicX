2045eb82b5b490fe3f31ade5f3c3859633a2d33d
Suchith Prabhu <suchith720@gmail.com>
2025-08-15 10:18:52 +0000

    experiments to mitigate overfitting

--- command.sh
+++ command.sh
@@ -42,4 +42,4 @@ python mogicX/37_training-msmarco-distilbert-from-scratch-inference.py --use_sxc
 python mogicX/30_ngame-for-msmarco-with-hard-negatives-inference.py --use_sxc_sampler --pickle_dir /home/aiscuser/scratch1/datasets/processed/ --only_test --do_test_inference
 
 # Conflation code
-python mogicX/42_entity-conflation.py --pred_file /data/datasets/msmarco/XC/entity-gpt_ngame_tst_X_Y.npz --lbl_file //data/datasets/msmarco/XC/raw_data/entity_gpt.raw.txt  --trn_file /data/datasets/msmarco/XC/entity_gpt_trn_X_Y.npz --tst_file /data/datasets/msmarco/XC/entity_gpt_tst_X_Y.npz --topk 3 --batch_size 1024 --min_thresh 2 --max_thresh 100
+python mogicX/42_entity-conflation.py --pred_file /data/datasets/msmarco/XC/entity-gpt_ngame_trn_X_Y.npz --lbl_file //data/datasets/msmarco/XC/raw_data/entity_gpt.raw.txt  --trn_file /data/datasets/msmarco/XC/entity_gpt_trn_X_Y.npz --tst_file /data/datasets/msmarco/XC/entity_gpt_tst_X_Y.npz --topk 3 --batch_size 1024 --min_thresh 2 --max_thresh 100 --freq_thresh 50 --score_thresh 25 --diff_thresh 0.1 --embed_file /data/outputs/mogicX/01-msmarco-gpt-entity-linker-001/predictions/label_repr_full.pth

d08904555c8b9bf1bf34d54a9529be78b2290fb7
Suchith Prabhu <suchith720@gmail.com>
2025-10-19 03:09:09 +0000

    nvembed inference

--- scripts/15-nvembed_inference.sh
+++ scripts/15-nvembed_inference.sh
@@ -2,6 +2,8 @@
 
 for i in $(seq 0 13)
 do
-	CUDA_VISIBLE_DEVICES=$i python mogicX/54_nvembed-for-msmarco-embeddings-002.py --idx $i --parts 14 --get_lbl_repr &
+	# CUDA_VISIBLE_DEVICES=$i python mogicX/54_nvembed-for-msmarco-embeddings-002.py --idx $i --parts 14 --get_lbl_repr &
+
+	CUDA_VISIBLE_DEVICES=$i python mogicX/54_nvembed-for-msmarco-embeddings-002.py --idx $i --parts 14 --get_qry_repr &
 done
 wait

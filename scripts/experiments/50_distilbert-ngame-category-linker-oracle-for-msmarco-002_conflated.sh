5100aa41892b3da1bd8231d4c3079cf38068cf52
Suchith Prabhu <suchith720@gmail.com>
2025-10-22 13:52:58 +0000

    category experiment

--- scripts/experiments/50_distilbert-ngame-category-linker-oracle-for-msmarco-002_conflated.sh
+++ scripts/experiments/50_distilbert-ngame-category-linker-oracle-for-msmarco-002_conflated.sh
@@ -4,6 +4,10 @@ datasets="msmarco arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcor
 	cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers \
 	cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
 
+datasets="fever fiqa hotpotqa nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid \
+	cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers \
+	cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
+
 output_file=outputs/50_distilbert-ngame-category-linker-oracle-for-msmarco-002_conflated.txt
 for dataset in $datasets
 do

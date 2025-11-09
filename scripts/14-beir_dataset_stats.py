4664e319ecf3a5d8a1677468fbe8a2642c9e2300
Suchith Prabhu <suchith720@gmail.com>
2025-11-09 00:48:07 +0000

    more experiments

--- scripts/14-beir_dataset_stats.py
+++ scripts/14-beir_dataset_stats.py
@@ -1,23 +1,27 @@
-import scipy.sparse as sp
+import scipy.sparse as sp, os
 
 if __name__ == '__main__':
     
-    datasets="msmarco arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
+    datasets="msmarco arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq nq-train quora scidocs scifact webis-touche2020 trec-covid cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
 
     total_trn, total_lbls = 0, 0
 
     for dset in datasets.split(' '):
         print(f'{dset}:')
         trn_file = f"/data/datasets/beir/{dset}/XC/trn_X_Y.npz"
-        trn_lbl = sp.load_npz(trn_file)
-        print(f'Train shape: {trn_lbl.shape[0]} x {trn_lbl.shape[1]}')
 
-        total_trn += trn_lbl.shape[0]
-        total_lbls += trn_lbl.shape[1]
+        if os.path.exists(trn_file):
+            trn_lbl = sp.load_npz(trn_file)
+            print(f'Train shape: {trn_lbl.shape[0]} x {trn_lbl.shape[1]}')
+
+            total_trn += trn_lbl.shape[0]
+            total_lbls += trn_lbl.shape[1]
 
         tst_file = f"/data/datasets/beir/{dset}/XC/tst_X_Y.npz"
-        tst_lbl = sp.load_npz(tst_file)
-        print(f'Test shape: {tst_lbl.shape[0]} x {tst_lbl.shape[1]}')
+        if os.path.exists(tst_file):
+            tst_lbl = sp.load_npz(tst_file)
+            print(f'Test shape: {tst_lbl.shape[0]} x {tst_lbl.shape[1]}')
+
         print()
 
     print(f'Total training points: {total_trn}')
--- scripts/17-combine_beir_datasets.py
+++ scripts/17-combine_beir_datasets.py
@@ -1,28 +1,11 @@
-import scipy.sparse as sp, os
+import scipy.sparse as sp, os, numpy as np, argparse
 
 from tqdm.auto import tqdm
 
 from sugar.core import *
 
-def load_data(dataset):
-    trn_raw_file = (
-        f"/data/datasets/beir/{dataset}/XC/raw_data/train.raw.txt"
-        if dataset == "msmarco" else
-        f"/data/datasets/beir/{dataset}/XC/raw_data/train.raw.csv"
-    )
-    trn_mat_file = f"/data/datasets/beir/{dataset}/XC/trn_X_Y.npz"
-    trn_ids, trn_txt = load_raw_file(trn_raw_file)
-    trn_mat = sp.load_npz(trn_mat_file)
 
-    tst_raw_file = (
-        f"/data/datasets/beir/{dataset}/XC/raw_data/test.raw.txt"
-        if dataset == "msmarco" else
-        f"/data/datasets/beir/{dataset}/XC/raw_data/test.raw.csv"
-    )
-    tst_mat_file = f"/data/datasets/beir/{dataset}/XC/tst_X_Y.npz"
-    tst_ids, tst_txt = load_raw_file(tst_raw_file)
-    tst_mat = sp.load_npz(tst_mat_file)
-
+def load_data(dataset, use_generated_queries=False):
     lbl_raw_file = (
         f"/data/datasets/beir/{dataset}/XC/raw_data/label.raw.txt"
         if dataset == "msmarco" else
@@ -30,14 +13,46 @@ def load_data(dataset):
     )
     lbl_ids, lbl_txt = load_raw_file(lbl_raw_file)
 
+    if use_generated_queries:
+        trn_raw_file = (
+            f"/data/datasets/beir/{dataset}/XC/raw_data/train_generated.raw.txt"
+            if dataset == "msmarco" else
+            f"/data/datasets/beir/{dataset}/XC/raw_data/train_generated.raw.csv"
+        )
+        trn_mat_file = f"/data/datasets/beir/{dataset}/XC/trn_X_Y_generated.npz"
+    else:
+        trn_raw_file = (
+            f"/data/datasets/beir/{dataset}/XC/raw_data/train.raw.txt"
+            if dataset == "msmarco" else
+            f"/data/datasets/beir/{dataset}/XC/raw_data/train.raw.csv"
+        )
+        trn_mat_file = f"/data/datasets/beir/{dataset}/XC/trn_X_Y.npz"
+
+    trn_ids, trn_txt, trn_mat = [], [], sp.csr_matrix((0, len(lbl_ids)), dtype=np.float32)
+    if os.path.exists(trn_mat_file):
+        trn_ids, trn_txt = load_raw_file(trn_raw_file)
+        trn_mat = sp.load_npz(trn_mat_file)
+
+    tst_raw_file = (
+        f"/data/datasets/beir/{dataset}/XC/raw_data/test.raw.txt"
+        if dataset == "msmarco" else
+        f"/data/datasets/beir/{dataset}/XC/raw_data/test.raw.csv"
+    )
+    tst_mat_file = f"/data/datasets/beir/{dataset}/XC/tst_X_Y.npz"
+
+    tst_ids, tst_txt, tst_mat = [], [], sp.csr_matrix((0, len(lbl_ids)), dtype=np.float32)
+    if os.path.exists(tst_mat_file):
+        tst_ids, tst_txt = load_raw_file(tst_raw_file)
+        tst_mat = sp.load_npz(tst_mat_file)
+
     return (trn_ids, trn_txt, trn_mat), (tst_ids, tst_txt, tst_mat), (lbl_ids, lbl_txt)
 
 
-def save_data(trn_info, tst_info, lbl_info):
-    data_dir = '/data/datasets/beir/all-beir/XC'
+def save_data(trn_info, tst_info, lbl_info, use_generated_queries=False):
+    data_dir = '/data/datasets/beir/all-beir-gen/XC' if use_generated_queries else '/data/datasets/beir/all-beir/XC'
     os.makedirs(data_dir, exist_ok=True)
 
-    raw_dir = '/data/datasets/beir/all-beir/XC/raw_data'
+    raw_dir = '/data/datasets/beir/all-beir-gen/XC/raw_data' if use_generated_queries else '/data/datasets/beir/all-beir/XC/raw_data'
     os.makedirs(raw_dir, exist_ok=True)
 
     # train
@@ -59,9 +74,16 @@ def save_data(trn_info, tst_info, lbl_info):
     save_raw_file(lbl_raw_file, lbl_info[0], lbl_info[1])
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use_generated_queries', action="store_true")
+    return parser.parse_known_args()[0]
+
 
 if __name__ == "__main__":
-    datasets = "msmarco arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
+    args = parse_args()
+
+    datasets = "msmarco arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq nq-train quora scidocs scifact webis-touche2020 trec-covid cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
     datasets = datasets.split(" ")
 
     trn_ids, trn_txt = [], []
@@ -70,8 +92,9 @@ if __name__ == "__main__":
 
     trn_mat, tst_mat = [], []
 
+    names = list()
     for dataset in tqdm(datasets):
-        trn, tst, lbl = load_data(dataset)
+        trn, tst, lbl = load_data(dataset, args.use_generated_queries)
 
         lbl_ids.extend(lbl[0])
         lbl_txt.extend(lbl[1])
@@ -84,6 +107,11 @@ if __name__ == "__main__":
 
         trn_mat.append(trn[2])
         tst_mat.append(tst[2])
+
+        if len(trn[0]): names.append(dataset)
+
+    name = ", ".join(names)
+    print(f"Datasets with training data: {name}")
 
     def combine_mat(mats):
         num_lbls, resz_mats = 0, []
@@ -100,7 +128,5 @@ if __name__ == "__main__":
     assert len(tst_ids) == tst_mat.shape[0]
     assert len(lbl_ids) == trn_mat.shape[1] == tst_mat.shape[1]
 
-    save_data((trn_ids, trn_txt, trn_mat), (tst_ids, tst_txt, tst_mat), (lbl_ids, lbl_txt))
-
-
+    save_data((trn_ids, trn_txt, trn_mat), (tst_ids, tst_txt, tst_mat), (lbl_ids, lbl_txt), use_generated_queries=args.use_generated_queries)
 
